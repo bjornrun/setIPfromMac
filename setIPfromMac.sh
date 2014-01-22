eb84c69fd3a44473b0ee24310955885dbc50cec2
Bjorn Runaker <bjornrun@gmail.com>
2014-01-22 14:03:33 +0100

    Added missing network parameters
    Make sure localhost network is up
    Set the default GW routing

--- setIPfromMac.sh
+++ setIPfromMac.sh
@@ -1,17 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-IFACE=tap0
-IP_TEMPL="10.1.1.%d\n"
+IFACE=eth0
+IP_TEMPL="10.10.61.%d\n"
 NETMASK="255.255.255.252"
 BROADCASTMASK=3
+IPDELTA2GW=-1
 
 HEXNUM4MAC=$(grep -oE "[^:]+$" /sys/class/net/$IFACE/address)
 NUM4MAC=$(printf "%d\n" 0x$HEXNUM4MAC)
 
-
 BROADCASTADDR=$(($NUM4MAC | $BROADCASTMASK))
-
 NEWBROADCAST=$(printf $IP_TEMPL $BROADCASTADDR) 
 NEWIP=$(printf $IP_TEMPL $NUM4MAC)
+GWIP=$(printf $IP_TEMPL $(($NUM4MAC + $IPDELTA2GW)))
+
 ifconfig $IFACE $NEWIP netmask $NETMASK broadcast $NEWBROADCAST
- 
+
+ifconfig lo up
+
+route add default gw $GWIP
+
